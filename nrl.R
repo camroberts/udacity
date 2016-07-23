66f813aaf329ef5ea4aee98c041573a32dfcbc3b
Cameron Roberts <cam.d.roberts@gmail.com>
2016-07-23 17:45:44 +1000

    First round of data prep

--- nrl.R
+++ nrl.R
@@ -11,65 +11,68 @@ nrl_long <- nrl_long[complete.cases(nrl_long),]
 nrl_long$Year <- substr(nrl_long$Year, 2, 5)
 write.csv(nrl_long, 'data/nrl_long.csv')
 
-nrl <- spread(nrl_long, 'Year', 'Position')
-write.csv(nrl, 'data/nrl.csv', na = "", row.names = FALSE)
+nrl <- spread(nrl_long, 'Team', 'Position')
+nrl <- data.frame(nrl[,-1], row.names = nrl[,1])
+write.csv(nrl, 'data/nrl.csv', na = "")
 
 # There must be a way to do all these calcs without loops in R, 
 # but I don't have the time to figure it out today.
 
 # No. years since last premiership ---------------
 years_since_1 <- nrl
-for (i in 2:ncol(years_since_1)) {
-  prem <- !is.na(years_since_1[,i]) & years_since_1[,i] == 1
-  notPrem <- !is.na(years_since_1[,i]) & years_since_1[,i] != 1
-  years_since_1[prem,i] <- 0
-  if (i == 2) {
-    years_since_1[notPrem,i] <- 1
+for (i in 1:nrow(years_since_1)) {
+  prem <- !is.na(years_since_1[i,]) & years_since_1[i,] == 1
+  notPrem <- !is.na(years_since_1[i,]) & years_since_1[i,] != 1
+  years_since_1[i,prem] <- 0
+  if (i == 1) {
+    years_since_1[i,notPrem] <- 1
   } else {
-    prev <- years_since_1[,i-1]
+    prev <- years_since_1[i-1,]
     prev[is.na(prev)] <- 0
-    years_since_1[notPrem,i] <- prev[notPrem] + 1
+    years_since_1[i,notPrem] <- prev[notPrem] + 1
   }
 }
+mutate(years_since_1, mean = rowMeans(years_since_1, na.rm = TRUE))
+
+mean_years <- rowMeans(years_since_1, na.rm = TRUE)
+
+# No. years since current premier won ---------------
+prem <- which(years_since_1 == 0, arr.ind = TRUE)
+prem[,1] <- prem[,1]-1
+prem['1908',1] <- 1
+prem <- prem[order(rownames(prem)),]
+years_between <- data.frame(years_since_1[prem], row.names = row.names(prem))
+years_between[is.na(years_between)] <- 0
 
 # No. unique premiers in last n-years -----------------
 n <- 10
-uq_prems <- nrl[1,-1]
-for (i in 2:ncol(nrl)) {
-  if (i == 2) {
-    uq_prems[i-1] <- 1
+uq_prems <- data.frame(integer(nrow(nrl)), row.names = rownames(nrl))
+for (i in 1:nrow(nrl)) {
+  if (i == 1) {
+    uq_prems[i,] <- 1
   } else {
-    prems <- which(nrl[,(i-(min(i-1,n)-1)):i] == 1, arr.ind = TRUE)
-    uq_prems[i-1] <- length(unique(prems[,1]))  
-  }
-}
-
-# Perc of teams premiers in last n-years -----------------
-n <- 10
-prem_perc <- nrl[1,-1]
-for (i in 2:ncol(nrl)) {
-  if (i == 2) {
-    prem_perc[i-1] <- 1/sum(!is.na(nrl[,i]))
-  } else {
-    results <- nrl[,(i-(min(i-1,n)-1)):i]
-    prems <- which(results == 1, arr.ind = TRUE)
-    teams <- which(!is.na(results), arr.ind = TRUE)
-    prem_perc[i-1] <- length(unique(prems[,1]))/length(unique(teams[,1]))
+    prems <- which(nrl[(i-(min(i,n)-1)):i,] == 1, arr.ind = TRUE)
+    uq_prems[i,] <- length(unique(prems[,2]))  
   }
 }
 
 # Perc of possible premiers in last n-years -----------------
 n <- 10
-prem_perc <- nrl[1,-1]
-for (i in 2:ncol(nrl)) {
-  if (i == 2) {
-    prem_perc[i-1] <- 1/min(n, sum(!is.na(nrl[,i])))
+prem_perc <- data.frame(integer(nrow(nrl)), row.names = rownames(nrl))
+for (i in 1:nrow(nrl)) {
+  if (i == 1) {
+    prem_perc[i,] <- 1/min(n, sum(!is.na(nrl[i,])))
   } else {
-    results <- nrl[,(i-(min(i-1,n)-1)):i]
+    results <- nrl[(i-(min(i,n)-1)):i,]
     prems <- which(results == 1, arr.ind = TRUE)
     teams <- which(!is.na(results), arr.ind = TRUE)
-    prem_perc[i-1] <- length(unique(prems[,1]))/min(n, length(unique(teams[,1])))
+    prem_perc[i,] <- length(unique(prems[,2]))/min(n, length(unique(teams[,2])))
   }
 }
+
+yearly_summary <- data.frame(years_between, mean_years, uq_prems, prem_perc)
+colnames(yearly_summary) <- c('years_between', 'mean_years', 'unique_prems', 
+                              'prem_perc')
+write.csv(yearly_summary, 'data/nrl_yearly_stats.csv', na = "")
 
 # Distribution of results --------------
